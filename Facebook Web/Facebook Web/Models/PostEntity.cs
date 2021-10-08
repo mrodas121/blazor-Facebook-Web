36bcc7df2d8ca0906932c22085595533e72fb157
mrodas121 <manuel.rodas121@gmail.com>
2021-10-07 22:28:41 -0600

    finalizacion de maquetacion

---
+++ Facebook Web/Facebook Web/Models/PostEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Facebook_Web.Models
+{
+    public class PostEntity
+    {
+        public string ImageB64 { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+        public string Comments { get; set; }
+        public string Reactions { get; set; }
+        public PostEntity(string imageB64, string name, string date, string description, string comments, string reactions)
+        {
+            ImageB64 = imageB64;
+            Name = name;
+            Date = date;
+            Description = description;
+            Comments = comments;
+            Reactions = reactions;
+        }
+    }
+}
